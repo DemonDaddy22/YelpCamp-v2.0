eca378e6d078628f069383f4f0ced72b65f531f1
Rohan Gupta <guptarohan369@gmail.com>
2021-03-10 16:10:19 +0530

    Initial code setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.listen(3000, () => console.log('> Server started on Port: 3000'));
