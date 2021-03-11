b0dc7edd2e6423f83ad2c11a5afb6f6226283277
Rohan Gupta <guptarohan369@gmail.com>
2021-03-12 00:39:34 +0530

    Setup get routes for all campgrounds and individual campgrounds

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/Campground');
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
@@ -11,7 +12,18 @@ db.once('open', () => console.log('> Database connection established'));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`> Server started on Port: ${PORT}`));
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.listen(PORT, () => console.log(`> Server started on Port: ${PORT}`));
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/details', { campground });
+});
