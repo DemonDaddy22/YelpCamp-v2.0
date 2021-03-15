02b075481c1fcf9f9c9363169a3763e35dd5f4a1
Rohan Gupta <guptarohan369@gmail.com>
2021-03-16 00:29:14 +0530

    Update campground forms' styles

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 3000;
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
