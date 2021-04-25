b3008d11f1de3faca952a2fa046f1684ec002fd4
Rohan Gupta <guptarohan369@gmail.com>
2021-04-25 15:41:27 +0530

    Throw custom error for duplicate email while registering new user

--- models/User.js
+++ models/User.js
@@ -14,8 +14,13 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: [true, 'Email is required'],
-        unique: true
-    }
+        unique: true,
+    },
+});
+
+UserSchema.post('save', (err, _, next) => {
+    if (err.name === 'MongoError' && err.code === 11000) next(new Error('A user with the given email is already registered'));
+    else next();
 });
 
 // passportLocalMongoose automatically assigns a unique username, hashed password and salt value to the user model behind the scenes
