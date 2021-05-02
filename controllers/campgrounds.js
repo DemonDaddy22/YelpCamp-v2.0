a56fc6288c3e7b28a844bdcf1ce43b971752aae3
Rohan Gupta <guptarohan369@gmail.com>
2021-05-02 18:57:22 +0530

    Add map for entered location by finding geolocation of location

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,9 @@
 const Campground = require('../models/Campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const { cloudinary } = require('../cloudinary-config');
+
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 const getAllCamprounds = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -24,7 +28,12 @@ const getCampground = async (req, res) => {
 };
 
 const createNewCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1,
+    }).send();
     const campground = new Campground(req.body.campground);
+    campground.geoLocation = geoData.body.features[0].geometry;
     campground.author = req.user._id;
     campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
     const response = await campground.save();
@@ -40,10 +49,15 @@ const getEditCampgroundForm = async (req, res) => {
 
 const editCampground = async (req, res) => {
     const { id } = req.params;
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1,
+    }).send();
     const campground = await Campground.findByIdAndUpdate(id, req.body.campground, {
         new: true,
         runValidators: true,
     });
+    campground.geoLocation = geoData.body.features[0].geometry;
     campground.images.push(...req.files.map(file => ({ url: file.path, filename: file.filename })));
     await campground.save();
     if (req.body.deleteImages) {
--- models/Campground.js
+++ models/Campground.js
@@ -29,6 +29,17 @@ const CampgroundSchema = new Schema({
     location: {
         type: String,
     },
+    geoLocation: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
