6435e4b3770934f3a7f7c0cdf12359c07727e347
Rohan Gupta <guptarohan369@gmail.com>
2021-05-08 12:10:45 +0530

    Add helmet

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 const YelpCampError = require('./utils/YelpCampError');
 const { setLocals } = require('./utils/middleware');
 const User = require('./models/User');
@@ -41,13 +42,57 @@ app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(helmet());
+
+const scriptSrcUrls = [
+    'https://stackpath.bootstrapcdn.com',
+    'https://api.tiles.mapbox.com',
+    'https://api.mapbox.com',
+    'https://kit.fontawesome.com',
+    'https://cdnjs.cloudflare.com',
+    'https://cdn.jsdelivr.net',
+];
+const styleSrcUrls = [
+    'https://kit-free.fontawesome.com',
+    'https://stackpath.bootstrapcdn.com',
+    'https://api.mapbox.com',
+    'https://api.tiles.mapbox.com',
+    'https://fonts.googleapis.com',
+    'https://use.fontawesome.com',
+    'https://cdn.jsdelivr.net/',
+];
+const connectSrcUrls = ['https://api.mapbox.com', 'https://*.tiles.mapbox.com', 'https://events.mapbox.com'];
+const fontSrcUrls = ['https://cdn.jsdelivr.net/'];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", 'blob:'],
+            childSrc: ['blob:'],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                'blob:',
+                'data:',
+                'https://res.cloudinary.com/yelp-camp/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                'https://images.unsplash.com',
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 const sessionConfig = {
+    name: 'session',
     secret: 'canbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     },
