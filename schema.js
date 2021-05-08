c736bf413dac5a1f7489a2be7d4dedfb249bb717
Rohan Gupta <guptarohan369@gmail.com>
2021-05-08 10:32:37 +0530

    Sanitize html and requests to server

--- schema.js
+++ schema.js
@@ -1,25 +1,48 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 const campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().min(0).required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
+        description: Joi.string().required().escapeHTML(),
+        location: Joi.string().required().escapeHTML(),
     }).required(),
     deleteImages: Joi.array(),
 });
 
 const reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5),
     }).required(),
 });
 
 const commentSchema = Joi.object({
     comment: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
     }).required(),
 });
 
